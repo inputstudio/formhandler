fbc8beb503af36e6a41eb2a955f671d7c8895b99
Alex N'Guessan <alexmarcaureln@gmail.com>
2022-06-13 18:29:56 +0000

    WIP: Send message to Telegram

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-  async fetch(request) {
-    return new Response("Hello World!");
+  async fetch(request, env) {
+    if (request.method !== "POST") {
+      return new Response("Expected POST", { status: 500 });
+    }
+
+    const { name, email, subject, message } = await request.json();
+
+    if (!name || !email || !subject || !message) {
+      return new Response("Please fill requiered fields ", { status: 400 });
+    }
+
+    const { BOT_API_KEY, CHAT_ID } = env;
+
+    if (!BOT_API_KEY || !CHAT_ID) {
+      return new Response(env, { status: 500 });
+      // return new Response("Missing environment variables", { status: 500 });
+    }
+
+    const text = `We received a message from : ${fullname} (${email})`;
+
+    try {
+      const telegramResponse = await fetch(
+        `https://api.telegram.org/bot${BOT_API_KEY}/sendMessage?chat_id=${CHAT_ID}&text=${text}`
+      );
+
+      return new Response("Message sended successfully !");
+    } catch (error) {
+      return new Response("An error occured, please try later.", {
+        status: 500,
+      });
+    }
   },
 };
